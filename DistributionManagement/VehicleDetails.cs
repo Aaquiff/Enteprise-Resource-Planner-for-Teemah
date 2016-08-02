adf26147cf391107f24e2bc92872e517fbb777a3
Aaquiff <aaquiff@gmail.com>
2016-08-02 19:44:59 +0530

    Adding Distribution forms

---
+++ DistributionManagement/VehicleDetails.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DistributionManagement
+{
+    public partial class VehicleDetails : Form
+    {
+        public VehicleDetails()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
