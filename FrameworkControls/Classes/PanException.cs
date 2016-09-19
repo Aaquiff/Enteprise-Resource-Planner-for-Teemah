abedcbac9eb7f0074ff2fbd131dc625283366a72
Aaquiff <aaquiff@gmail.com>
2016-09-19 23:22:09 +0530

    19-09-2016

---
+++ FrameworkControls/Classes/PanException.cs
@@ -0,0 +1,18 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FrameworkControls.Classes
+{
+    public static class PanException
+    {
+        public static void Show(IWin32Window owner,MySqlException ex)
+        {
+            MetroFramework.MetroMessageBox.Show(owner,ex.Message,"Error",MessageBoxButtons.OK,MessageBoxIcon.Error);
+        }
+    }
+}
--- RawMaterialManagement/RawMaterialDashBoard.cs
+++ RawMaterialManagement/RawMaterialDashBoard.cs
@@ -7,14 +7,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using FrameworkControls.Classes;
 
 namespace RawMaterialManagement
 {
     public partial class RawMaterialDashBoard : Form
     {
+        MySqlConnection con;
         public RawMaterialDashBoard()
         {
             InitializeComponent();
+            con = MySQLDatabaseAccess.Connection.getConnection();
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -24,10 +28,19 @@ namespace RawMaterialManagement
 
         private void RawMaterialDashBoard_Load(object sender, EventArgs e)
         {
-            // TODO: This line of code loads data into the 'rawDataSet.raw_purchase_order' table. You can move, or remove it, as needed.
-            this.raw_purchase_orderTableAdapter.Fill(this.rawDataSet.raw_purchase_order);
-            // TODO: This line of code loads data into the 'rawDataSet.raw_item_tab' table. You can move, or remove it, as needed.
-            this.raw_item_tabTableAdapter.Fill(this.rawDataSet.raw_item_tab);
+            try
+            {
+                // TODO: This line of code loads data into the 'rawDataSet.raw_purchase_order' table. You can move, or remove it, as needed.
+                this.raw_purchase_orderTableAdapter.Fill(this.rawDataSet.raw_purchase_order);
+                // TODO: This line of code loads data into the 'rawDataSet.raw_item_tab' table. You can move, or remove it, as needed.
+                MySqlDataAdapter itemAdapter = new MySqlDataAdapter("select * from raw_item_tab order by stock_level desc limit 10",con);
+                itemAdapter.Fill(rawDataSet.raw_item_tab);
+                //this.raw_item_tabTableAdapter.Fill(this.rawDataSet.raw_item_tab);
+            }
+            catch (MySqlException ex)
+            {
+                PanException.Show(this,ex);
+            }
 
         }
     }
