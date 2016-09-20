770041329bc761b40908887e2e1d26dc8f0c8742
KiruthigaBalendran <kiruthiga03@gmail.com>
2016-09-20 23:14:11 +0530

    Commiting PanMessage and removed signature from project

---
+++ FrameworkControls/Classes/PanMessage.cs
@@ -0,0 +1,22 @@
+﻿using MetroFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FrameworkControls.Classes
+{
+    public static class PanMessage
+    {
+        public static void Show(IWin32Window owner,string message)
+        {
+            MetroMessageBox.Show(owner,message,"Success",MessageBoxButtons.OK,MessageBoxIcon.Question);
+        }
+        public static void Show(IWin32Window owner, string title, string message)
+        {
+            MetroMessageBox.Show(owner, message, title, MessageBoxButtons.OK, MessageBoxIcon.Question);
+        }
+    }
+}
