281efa520b7787f6538b3cd6fefbf4453dd9e0cb
KiruthigaBalendran <kiruthiga03@gmail.com>
2016-09-24 00:34:57 +0530

    Final HR

---
+++ DB Script/HR Management/leave insert.sql
@@ -0,0 +1,15 @@
+/*
+-- Query: SELECT * FROM itp.`leave`
+LIMIT 0, 1000
+
+-- Date: 2016-09-24 00:30
+*/
+INSERT INTO `leave` (`LeaveId`,`EmpId`,`RequestedDate`,`StartDate`,`EndDate`,`Type`,`Reason`,`Status`) VALUES (1,1,'2016-05-16','2016-05-18','2016-05-20','Annual','Exams','Accepted');
+INSERT INTO `leave` (`LeaveId`,`EmpId`,`RequestedDate`,`StartDate`,`EndDate`,`Type`,`Reason`,`Status`) VALUES (2,8,'2016-05-28','2016-05-30','2016-06-01','Casual','Sick','Denied');
+INSERT INTO `leave` (`LeaveId`,`EmpId`,`RequestedDate`,`StartDate`,`EndDate`,`Type`,`Reason`,`Status`) VALUES (3,8,'2016-06-15','2016-06-18','2016-06-25','Annual','Marriage','Accepted');
+INSERT INTO `leave` (`LeaveId`,`EmpId`,`RequestedDate`,`StartDate`,`EndDate`,`Type`,`Reason`,`Status`) VALUES (4,5,'2016-07-24','2016-07-25','2016-07-27','Annual','Sick','Denied');
+INSERT INTO `leave` (`LeaveId`,`EmpId`,`RequestedDate`,`StartDate`,`EndDate`,`Type`,`Reason`,`Status`) VALUES (6,7,'2016-08-12','2016-08-22','2016-08-24','Casual','Trip','Accepted');
+INSERT INTO `leave` (`LeaveId`,`EmpId`,`RequestedDate`,`StartDate`,`EndDate`,`Type`,`Reason`,`Status`) VALUES (7,4,'2016-08-22','2016-08-22','2016-08-25','Annual','Funeral','Denied');
+INSERT INTO `leave` (`LeaveId`,`EmpId`,`RequestedDate`,`StartDate`,`EndDate`,`Type`,`Reason`,`Status`) VALUES (8,4,'2016-08-23','2016-08-23','2016-08-25','Annual','Dead','Denied');
+INSERT INTO `leave` (`LeaveId`,`EmpId`,`RequestedDate`,`StartDate`,`EndDate`,`Type`,`Reason`,`Status`) VALUES (11,6,'2016-08-23','2016-08-23','2016-08-27','Casual','Trip','Denied');
+INSERT INTO `leave` (`LeaveId`,`EmpId`,`RequestedDate`,`StartDate`,`EndDate`,`Type`,`Reason`,`Status`) VALUES (12,6,'2016-08-24','2016-08-24','2016-08-27','Annual','Wedding','Denied');
